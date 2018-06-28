09e45274bdb39e463f2f2c85fc5c42d8748f29b7
Christopher Liu <chrisliu@berkeley.edu>
2018-06-28 12:15:51 -0700

    oop2

--- add_gender.py
+++ add_gender.py
@@ -10,13 +10,14 @@ for profile in data:
     profile['parsed']['?gender_male'] = 0
     profile['parsed']['?gender_female'] = 0
     profile['parsed']['?gender_other'] = 0
-    gender = profile['raw']['gender']
-    if profile['raw']['gender'].lower() == 'male':
-        profile['parsed']['?gender_male'] = 1
-    elif profile['raw']['gender'].lower() == 'female':
-        profile['parsed']['?gender_female'] = 1
-    else:
-        profile['parsed']['?gender_other'] = 1
+    if profile['parsed']['!gender']:
+        gender = profile['raw']['gender']
+        if profile['raw']['gender'].lower() == 'male':
+            profile['parsed']['?gender_male'] = 1
+        elif profile['raw']['gender'].lower() == 'female':
+            profile['parsed']['?gender_female'] = 1
+        else:
+            profile['parsed']['?gender_other'] = 1
 
 with open(filepath, 'w') as file:
     json.dump(data, filepath)
